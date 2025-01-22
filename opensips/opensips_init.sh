5d29ae045b70562291596d91d36a815d314e2883
WalidFerjani2 <walid.ferjani1998@gmail.com>
2025-01-22 10:20:43 +0100

    Changed opensips config

--- opensips/opensips_init.sh
+++ opensips/opensips_init.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
+# Create OpenSIPS database
 opensipsdbctl create <<EOF
-
+# If any database configuration is required, add here
 EOF
 
-
+# Wait a moment for db setup
 sleep 1
+
+# Create group and user for OpenSIPS
 groupadd opensips
-sleep 2
 useradd -g opensips opensips
 sleep 3
+
+# Make sure the OpenSIPS service script is executable
 chmod +x /etc/init.d/opensips
+
+# Ensure OpenSIPS log directory exists
+mkdir -p /var/log/opensips
+touch /var/log/opensips.log
+chown opensips:opensips /var/log/opensips /var/log/opensips.log
 sleep 1
 
+# Restart OpenSIPS if needed
+# /etc/init.d/opensips restart
 
+sleep 1
 
--- opensips/start_opensips.sh
+++ opensips/start_opensips.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
+
+# Substitute the values for PRESENCE_IP and IMS_DOMAIN in the configuration files
 sed -i 's|PRESENCE_IP|'$PRESENCE_IP'|g' /etc/opensips/opensips.cfg
 sleep 1
 sed -i 's|IMS_DOMAIN|'$IMS_DOMAIN'|g' /etc/opensips/opensipsctlrc
 sleep 1
+
+# Restart syslog-ng if necessary (you may modify this depending on your logging setup)
+# Not mandatory in Docker, but you can use it to log inside the container
 /etc/init.d/syslog-ng restart
 sleep 1
-/etc/init.d/mysql restart 
+
+# Restart MySQL (in case it was not properly initialized or needs a restart)
+# MySQL is required by OpenSIPS for storing registrations, presence, etc.
+# Only restart if it's necessary. If OpenSIPS is handling the MySQL server internally, this is fine.
+# If you are using Docker and have a separate MySQL container, you may not need to do this.
+echo "Restarting MySQL..."
+/etc/init.d/mysql restart
 sleep 1
+
+# Start OpenSIPS after MySQL is ready
 opensipsctl start
 sleep 1
-tail -f /var/log/syslog 
+
+# Tail OpenSIPS log for monitoring the process (you can change this to /var/log/opensips.log if that's the correct log file)
+tail -f /var/log/opensips.log
